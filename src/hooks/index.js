3c0fe1c5a079b7ee886f673b7a19bb8c38355eee
abigailNottingham <abigailillustration@gmail.com>
2021-06-10 21:46:09 +0100

    ex 7.4-7.6 created custom useField hook and simplified inputs, separated reset function through destructuring

---
+++ src/hooks/index.js
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+export const useField = (type) => {
+  const [value, setValue] = useState("")
+
+  const onChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const reset = () => {
+    setValue("")
+  }
+
+  return [
+    reset,
+    {
+      type,
+      value,
+      onChange,
+    }
+  ]
+}
